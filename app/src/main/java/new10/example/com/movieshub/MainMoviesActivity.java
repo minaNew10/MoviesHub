2ce7434df90dac8da5f94ecf003543d5d23250c5
MinaN <minadevdream@gmail.com>
2019-08-30 13:38:50 +0200

    remove fav activity

--- app/src/main/java/new10/example/com/movieshub/MainMoviesActivity.java
+++ app/src/main/java/new10/example/com/movieshub/MainMoviesActivity.java
@@ -39,21 +39,29 @@ public class MainMoviesActivity extends AppCompatActivity implements MainMoviesA
     private static final String TAG = "MainMoviesList";
     @BindView(R.id.recycler_view_movies)
     RecyclerView recyclerView;
-    List<Movie> movies = new ArrayList<>();
+    List<Movie> movies;
     MainMoviesAdapter moviesAdapter;
     AppDatabase appDatabase;
     MainMoviesViewModel viewModel;
     Toast toast ;
-
+    ActionBar actionBar;
+    String title;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        actionBar = getSupportActionBar();
+        title = actionBar.getTitle().toString();
+        if(savedInstanceState != null){
+            title = savedInstanceState.getString(getString(R.string.key_for_title));
+            actionBar.setTitle(title);
+        }
         appDatabase = AppDatabase.getInstance(getApplicationContext());
         toast = Toast.makeText(this,getString(R.string.network_err),Toast.LENGTH_LONG);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, getResources().getInteger(R.integer.photo_list_preview_columns));
         moviesAdapter = new MainMoviesAdapter(movies,this);
+        setupViewModel();
         recyclerView.setAdapter(moviesAdapter);
         recyclerView.setLayoutManager(layoutManager);
         int orientation = this.getResources().getConfiguration().orientation;
@@ -69,7 +77,6 @@ public class MainMoviesActivity extends AppCompatActivity implements MainMoviesA
                     getResources().getInteger(R.integer.photo_list_preview_columns)));
         }
 
-        setupViewModel();
     }
 
     @Override
@@ -92,32 +99,45 @@ public class MainMoviesActivity extends AppCompatActivity implements MainMoviesA
     }
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        title = actionBar.getTitle().toString();
+        outState.putString(getString(R.string.key_for_title),title);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if(id == R.id.action_sort){
+
             //check sorting method
             if(item.getTitle().toString().equals(getString(R.string.sort_by_rating))) {
-                item.setTitle(getString(R.string.sort_by_popularity));
                 if (QueryUtils.isOnline(this)) {
+                    actionBar.setTitle(getString(R.string.app_name));
+                    item.setTitle(getString(R.string.sort_by_popularity));
                     //set multable live data to null to push the viewmodel to query again as in getRatedMovies() is doesn't query unless the list is null
                     viewModel.setMutableLiveData(null);
                     viewModel.getRatedMovies().observe(this, new Observer<MoviesList>() {
                         @Override
                         public void onChanged(MoviesList moviesList) {
-                            moviesAdapter.setMovies(moviesList.getResults());
+                            movies = moviesList.getResults();
+                            moviesAdapter.setMovies(movies);
                         }
                     });
                 }else {
                     toast.show();
                 }
             }else {
-                item.setTitle(getString(R.string.sort_by_rating));
                 if(QueryUtils.isOnline(this)) {
+                    item.setTitle(getString(R.string.sort_by_rating));
+                    actionBar.setTitle(getString(R.string.app_name));
+
                     viewModel.setMutableLiveData(null);
                     viewModel.getPopularMovies().observe(this, new Observer<MoviesList>() {
                         @Override
                         public void onChanged(MoviesList moviesList) {
-                            moviesAdapter.setMovies(moviesList.getResults());
+                            movies = moviesList.getResults();
+                            moviesAdapter.setMovies(movies);
                         }
                     });
                 }else {
@@ -126,23 +146,55 @@ public class MainMoviesActivity extends AppCompatActivity implements MainMoviesA
 
             }
         }else if(id == R.id.action_fav){
-            Intent intent = new Intent(MainMoviesActivity.this,FavouritesActivity.class);
-            startActivity(intent);
+                actionBar.setTitle(getString(R.string.favourites));
+                viewModel.getFavMovies(this).observe(this, new Observer<List<Movie>>() {
+                    @Override
+                    public void onChanged(List<Movie> favMovies) {
+                        if(favMovies == null || favMovies.size() == 0){
+                            toast.setText(getString(R.string.no_fav_movies));
+                            toast.show();
+                        }
+                        //this check to prevent getting back to favourites when choosing up after adding movie to favourites
+                        else if(actionBar.getTitle().equals(getString(R.string.favourites))) {
+                            movies = favMovies;
+                            moviesAdapter.setMovies(movies);
+                        }
+                    }
+                });
         }
         return true;
     }
+
     public void setupViewModel(){
         viewModel =  ViewModelProviders.of(this).get(MainMoviesViewModel.class);
-        if(QueryUtils.isOnline(this)) {
+        if(title.equals(getString(R.string.app_name))) {
             MutableLiveData<MoviesList> moviesList = viewModel.getPopularMovies();
             moviesList.observe(this, new Observer<MoviesList>() {
                 @Override
                 public void onChanged(MoviesList moviesList) {
-                    moviesAdapter.setMovies(moviesList.getResults());
+                    if(!QueryUtils.isOnline(MainMoviesActivity.this) && moviesList == null)
+                        toast.show();
+                    else {
+                        moviesAdapter.setMovies(moviesList.getResults());
+                    }
                 }
             });
-        }else {
-            moviesAdapter.setMovies(movies);
+        }else if( title.equals(getString(R.string.favourites))){
+            viewModel.getFavMovies(this).observe(this, new Observer<List<Movie>>() {
+                @Override
+                public void onChanged(List<Movie> favMovies) {
+                    if(favMovies == null || favMovies.size() == 0){
+                       toast.setText(getString(R.string.no_fav_movies));
+                       toast.show();
+                    }else {
+                        movies = favMovies;
+                        moviesAdapter.setMovies(movies);
+                    }
+                }
+            });
+
+        }else{
+            toast.setText(getString(R.string.network_err));
             toast.show();
         }
     }
--- app/src/main/java/new10/example/com/movieshub/ViewModel/MainMoviesViewModel.java
+++ app/src/main/java/new10/example/com/movieshub/ViewModel/MainMoviesViewModel.java
@@ -1,14 +1,24 @@
 package new10.example.com.movieshub.ViewModel;
 
+import android.content.Context;
+
+import new10.example.com.movieshub.Model.Movie;
 import new10.example.com.movieshub.Model.MoviesList;
+import new10.example.com.movieshub.Repository.FavMoviesRepository;
 import new10.example.com.movieshub.Repository.MainMoviesRepository;
+
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
 import new10.example.com.movieshub.Utils.QueryUtils;
 
 public class MainMoviesViewModel extends ViewModel {
 
     private MutableLiveData<MoviesList> mutableLiveData;
+    private LiveData<List<Movie>> favMovies;
 
     private MainMoviesRepository moviesRepository  = MainMoviesRepository.getInstance();
 
@@ -23,6 +33,13 @@ public class MainMoviesViewModel extends ViewModel {
             mutableLiveData = moviesRepository.getMovies(QueryUtils.QUERY_SORT_BY_RATING);
         }
         return mutableLiveData;
+    }
+
+    public LiveData<List<Movie>> getFavMovies(Context context){
+        if(favMovies == null){
+            favMovies = FavMoviesRepository.getFavMovies(context);
+        }
+        return favMovies;
     }
 
     public void setMutableLiveData(MutableLiveData<MoviesList> mutableLiveData) {
